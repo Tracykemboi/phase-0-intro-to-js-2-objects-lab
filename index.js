8dbcb389f2587d63aa84dfe7afa35106f128a167
Tracy Kemboi <tkemboi72@gmail.com>
2024-07-07 01:40:24 +0300

    modifyimg objects

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee=[]
+employee.name=David
+employee.streetAddress=nairobi
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+   const newObj={...employee}
+    newObj[key]= value
+    return newObj
+  }
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+  }
+  function deleteFromEmployeeByKey(employee,key,value) {
+   const newEmployeeObj= {...employee}
+    delete newEmployeeObj[key]
+    return newEmployeeObj
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee,key,value) {
+    delete employee[key]
+    return employee
+  }
